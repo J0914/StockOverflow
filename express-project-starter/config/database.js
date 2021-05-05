ad1fdf8cbaddbf6a8f6b63bbf3850dd96b34b8a7
J0914 <jordynsechrist@gmail.com>
2021-05-05 15:36:56 -0500

    make it better

--- express-project-starter/config/database.js
+++ express-project-starter/config/database.js
@@ -9,6 +9,10 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'postgres://ocyhcjtubhlcza:8dac876477f48f8e5e8f76ff80f04a877c1aebde3ffe3ee4cb41e91f34087400@ec2-52-87-107-83.compute-1.amazonaws.com:5432/dclfbaio1semk7',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
