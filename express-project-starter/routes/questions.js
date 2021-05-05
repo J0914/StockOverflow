93b8da54247dc06e9a2ec0de160499e68581f910
Andrew Loeffler <aloeffler@gmail.com>
2021-05-05 11:11:55 -0400

    add pug for question-thread, start question/id route

--- express-project-starter/routes/questions.js
+++ express-project-starter/routes/questions.js
@@ -6,7 +6,7 @@ const { asyncHandler } = require('./utils')
 router.get('/', asyncHandler(async(req,res) => {
     const questions = await db.Question.findAll({
         include: 'User'
-        // need to include QuestionVote so we can 
+        // need to include QuestionVote so we can
         // order by number of votes on this page.
     })
 
@@ -14,6 +14,19 @@ router.get('/', asyncHandler(async(req,res) => {
         title: 'Questions',
         questions
     })
+}))
+
+router.get('/:id(\\d+)', asyncHandler(async(req, res) => { //does this need a csrfToken for the new response form...?
+    const questionId = parseInt(req.params.id, 10);
+    const question = await db.Question.findByPk(questionId, {include: 'User'});
+    const responses = await db.Response.findAll({
+        where: {
+            questionId: questionId
+        },
+    })
+
+
+    res.render('question-thread', { responses, question })
 }))
 
 module.exports = router
