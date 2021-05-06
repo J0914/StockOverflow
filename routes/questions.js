55e5c0d9a5f64a750e6b9f1347171ace76227947
Andrew Loeffler <aloeffler@gmail.com>
2021-05-06 11:06:28 -0400

    finish pug for this page

--- routes/questions.js
+++ routes/questions.js
@@ -44,14 +44,14 @@ router.get('/', asyncHandler(async(req,res) => {
 router.get('/:id(\\d+)', asyncHandler(async(req, res) => { //does this need a csrfToken for the new response form...?
     const questionId = parseInt(req.params.id, 10);
     const question = await db.Question.findByPk(questionId, {include: 'User'});
-    const responses = await db.Response.findAll({
+    const allResponses = await db.Response.findAll({
         where: {
             questionId: questionId
         },
     })
+    const newResponse = await db.Response.build()
 
-
-    res.render('question-thread', { responses, question })
+    res.render('question-thread', { allResponses, question, response: newResponse })
 }))
 
 const questionValidators = [
