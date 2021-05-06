9bf6f4c20f02fe209156c08b474070c3a429b581
J0914 <jordynsechrist@gmail.com>
2021-05-05 21:53:15 -0500

    updated votes functionality

--- routes/questions.js
+++ routes/questions.js
@@ -14,17 +14,30 @@ router.get('/', asyncHandler(async(req,res) => {
             model: db.QuestionVote,
             as: 'QuestionVotes'
         }, {
-            model: db.QuestionVote,
-            as: 'QuestionVotes'
-        }, {
             model: db.Response,
             as: 'Responses'
-        }]
+        }],
     });
 
+    const findScore = (question) => {
+        const allVotes = question.QuestionVotes;
+        const totalScore = allVotes.reduce((accum, vote) => {
+          const score = vote.score;
+          accum += score;
+          return accum
+        }, 0) 
+        return totalScore
+    }
+    
+    questions.map(question => {
+        let score = findScore(question);
+        question['totalScore'] = score;
+    }) 
+
     res.render('questions', {
-        title: 'Questions',
-        questions
+        title: 'All Questions',
+        questions,
+        findScore
     })
 }))
 
@@ -61,7 +74,6 @@ router.get('/ask', csrfProtection, (req, res) => {
 
 
 router.post('/ask', csrfProtection, questionValidators, asyncHandler(async (req, res, next) => {
-    console.log(req.body)
 
     const { questionTitle, questionText, tags } = req.body;
 
