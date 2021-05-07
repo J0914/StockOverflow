1c817478a1ed0c4570897970277c9d2a468d9633
J0914 <jordynsechrist@gmail.com>
2021-05-07 16:35:30 -0500

    updated seeds

--- db/seeders/20210503221842-Tags.js
+++ db/seeders/20210503221842-Tags.js
@@ -4,7 +4,11 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Tags', [
       {name: 'crypto', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'blockChain', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'block-chain', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'stock market', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'data mining', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'mutual funds', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'other', createdAt: new Date(), updatedAt: new Date()},
     ])
     /*
       Add altering commands here.
