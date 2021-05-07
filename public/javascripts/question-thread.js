ffa46ecb0f539cf37e26f6ab9439926cc4a73485
victoriatarane <victoriatarane@gmail.com>
2021-05-06 17:01:15 -0700

    votes in progress

--- public/javascripts/question-thread.js
+++ public/javascripts/question-thread.js
@@ -1,11 +1,9 @@
 window.addEventListener("DOMContentLoaded", async () => {
     const scoreDiv = document.getElementById("totalScore");
     let score = scoreDiv.innerText;
-    console.log(score);
     const questionTitleDiv = document.querySelector('.questiontitle');
     const questionId = questionTitleDiv.id;
     const voteDiv = document.getElementById("voting-div");
-    console.log(voteDiv)
     voteDiv.addEventListener('click', async (event) => {
         if (event.target.id === "upvote-button"){
             const body = { score: 1 };
@@ -15,7 +13,8 @@ window.addEventListener("DOMContentLoaded", async () => {
                     "Content-Type": 'application/json',
                 },
                 body: JSON.stringify(body)
-            });            
+            });   
+                     
         }
         if (event.target.id === "downvote-button") {
             const body = { score: -1 };
