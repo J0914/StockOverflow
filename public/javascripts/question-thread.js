cfd86ed22900e79c1aafc2e0b90b6cfc561c0fc5
victoriatarane <victoriatarane@gmail.com>
2021-05-07 11:22:32 -0700

    Finished voted

--- public/javascripts/question-thread.js
+++ public/javascripts/question-thread.js
@@ -1,11 +1,16 @@
 window.addEventListener("DOMContentLoaded", async () => {
+    const upvote = document.getElementById("upvote-button");
+    const downvote = document.getElementById("downvote-button");
+    
     const scoreDiv = document.getElementById("totalScore");
     let score = scoreDiv.innerText;
     const questionTitleDiv = document.querySelector('.questiontitle');
     const questionId = questionTitleDiv.id;
     const voteDiv = document.getElementById("voting-div");
+    let counter = 1;
     voteDiv.addEventListener('click', async (event) => {
         if (event.target.id === "upvote-button"){
+            
             const body = { score: 1 };
             const response = await fetch(`${questionId}/vote`, {
                 method: 'POST',
@@ -14,12 +19,20 @@ window.addEventListener("DOMContentLoaded", async () => {
                 },
                 body: JSON.stringify(body)
             });   
-            const {totalScore} = await response.json();
+            const { totalScore } = await response.json();
             console.log(totalScore)
             scoreDiv.innerText = totalScore;
-            return totalScore;
+            counter++;
+            if (counter % 2 !== 1) {
+                upvote.innerText = "▲";
+                downvote.setAttribute("class", "hidden");
+            } else {
+                upvote.innerText = "△";
+                downvote.removeAttribute("class", "hidden");
+            }
         }
         if (event.target.id === "downvote-button") {
+            
             const body = { score: -1 };
             const response = await fetch(`${questionId}/vote`, {
                 method: 'POST',
@@ -31,7 +44,14 @@ window.addEventListener("DOMContentLoaded", async () => {
             const { totalScore } = await response.json();
             console.log(totalScore)
             scoreDiv.innerText = totalScore;
-            return totalScore;
+            counter++;
+            if (counter % 2 !== 1) {
+                downvote.innerText = "▼";
+                upvote.setAttribute("class", "hidden");
+            } else {
+                downvote.innerText = "▽";
+                upvote.removeAttribute("class", "hidden");
+            }
         }
         
     });
