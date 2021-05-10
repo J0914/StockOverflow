ed2dd571d33a9ea57bac904a324aa112303185e2
victoriatarane <victoriatarane@gmail.com>
2021-05-09 17:04:31 -0700

    final

--- routes/questions.js
+++ routes/questions.js
@@ -59,6 +59,9 @@ router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => { //do
         where: {
             questionId: questionId
         },
+        include: {
+            model: db.User
+        }
     });
     const newResponse = await db.Response.build();
     const questionVotes = await db.QuestionVote.findAll({ where: { questionId: question.id } });
@@ -146,11 +149,11 @@ router.post('/ask', csrfProtection, questionValidators, asyncHandler(async (req,
 
 router.post('/:id(\\d+)/vote', asyncHandler(async (req, res, next) => {
     const questionId = Number(req.params.id);
-    // console.log(req.body.responseId)
+    //console.log(req.body.responseId)
     const responseId = req.body.responseId;
     if (responseId !== undefined) {
-        console.log('here')
-        console.log(responseId)
+        //console.log('here')
+        //console.log(responseId)
         let allScores = await db.ResponseVote.findAll({ where: { responseId } });
         let totalResScore = 0;
         for (let i = 0; i < allScores.length; i++) {
@@ -183,7 +186,7 @@ router.post('/:id(\\d+)/vote', asyncHandler(async (req, res, next) => {
                 await currentVote.destroy();
             }
         } else {
-            res.redirect('/login');
+            console.error('Please log in!')
         }
         console.log(totalResScore)
         await res.json({ totalResScore })
@@ -220,9 +223,9 @@ router.post('/:id(\\d+)/vote', asyncHandler(async (req, res, next) => {
                 await currentVote.destroy();
             }
         } else {
-            res.redirect('/login');
+            console.error('Please log in!')
         }
-        console.log(totalScore);
+        //console.log(totalScore);
         await res.json({ totalScore });
     }
 }));
